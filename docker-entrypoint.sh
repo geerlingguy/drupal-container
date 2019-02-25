e0ccb6655cefc760d1f35b6b2eeac1f9f01d4ef5
Jeff Geerling <geerlingguy@mac.com>
2019-02-24 21:49:35 -0600

    Fixes #18: APACHE_DOCUMENT_ROOT can result in incorrect docroot.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,7 +15,6 @@ DRUPAL_DOWNLOAD_IF_NOT_PRESENT=${DRUPAL_DOWNLOAD_IF_NOT_PRESENT:-true}
 
 # Allow users to override the docroot by setting an environment variable.
 if [ ! -z "$APACHE_DOCUMENT_ROOT" ]; then
-  sed -ri -e "s|/var/www/html|$APACHE_DOCUMENT_ROOT|g" /etc/apache2/sites-available/*.conf
   sed -ri -e "s|/var/www/html|$APACHE_DOCUMENT_ROOT|g" /etc/apache2/sites-enabled/*.conf
   sed -ri -e "s!/var/www/!$APACHE_DOCUMENT_ROOT!g" /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
 fi
